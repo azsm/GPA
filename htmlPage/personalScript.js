4770294fade2002cd9ecf72aaa3723db18bffd4d
azsm <abouakil.zohour@gmail.com>
2016-04-03 22:09:01 +0200

    Create project

---
+++ htmlPage/personalScript.js
@@ -0,0 +1,9 @@
+// get the user input 
+$('#searchRepository').click(function(){
+    $('#retrievedRepositories').html($('#repositoryName').val());
+});
+
+
+// retrieve repositories list from the server 
+
+
