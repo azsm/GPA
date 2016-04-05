1ed79eddeb9009ef25a3108d6e6d564fa9273c3b
azsm <abouakil.zohour@gmail.com>
2016-04-05 11:45:54 +0200

    refactor the code

--- htmlPage/githubServiceCall.js
+++ htmlPage/githubServiceCall.js
@@ -1,19 +1,21 @@
-/*var URLS = (function() {
-     var private = {
-         'SEARCH_REPO_URL': 'https://api.github.com/search/repositories?q=',
-         'ANOTHER_CONST': '2'
-     };
+var URL_START = "https://api.github.com/";
 
-     return {
-        get: function(name) { return private[name]; }
-    };
-})();
-*/
+function getRepoSearchURL(name) { 
+    return URL_START + "search/repositories?q=" + name; 
+}
+        
+function getRepoContributorsURL(name) { 
+    return URL_START + "repos/" + name + "/stats/contributors";
+}
+
+function getRepoCommitsURL(name) {
+    return URL_START + "repos/" + name + "/stats/contributors";
+}
 
 
 function requestListRepositories(url, displayListRepository) {
 	$.get( url )
-		.done(displayListRepository)
+		.done( displayListRepository )
 		.fail(function() {
     		alert( "error contact admin" );
   		});
---
+++ htmlPage/handlerScript.js
@@ -0,0 +1,61 @@
+$('#searchRepository').submit(function(event){
+    var repoName = $('#repositoryName').val();
+    requestListRepositories(getRepoSearchURL(repoName), function(data) {
+	
+        $('#retrievedRepositoriesList').empty();
+
+        if (data.total_count > 0) {
+		    $.each(data.items, function(i, item) {
+   			    $('#retrievedRepositoriesList').append($('<option>', 
+                    { value: item.full_name, text : item.full_name }
+                ));
+		    });
+            $('#retrievedRepositoriesList').show();
+        }
+        else {
+            $('#retrievedRepositoriesList').hide();
+            alert("Not result found for this entry, please recheck !!");
+        }
+  	});
+    event.preventDefault();
+});
+
+
+$("#retrievedRepositories").click(function() {
+	var selectedRepo = $('#retrievedRepositoriesList option:selected').val();
+    requestListRepositories(getRepoContributorsURL(selectedRepo), function(data) {
+        var outhtml = '<p><strong>User List:</strong></p> <ul>';
+            
+        $.each(data.items, function(i, item) {
+			var contributor = item.author;
+            outhtml = outhtml + '<li><a href="'
+                        + contributor.html_url +'" target="_blank">'
+                        + contributor.login + '</a></li>';
+        });
+            
+		outhtml = outhtml + '</ul></div>'; 
+		$('#repoUsers').html(outhtml);
+
+  	});
+});
+
+/*
+$("#committersImpact").click(function() {
+    if($(this).is(":checked")) 
+    {
+        alert('The user want commits impact');
+    }
+});
+
+$("#commitTimeline").click(function() {
+    if($(this).is(":checked")) 
+    {
+        alert('The user want commit timeline');
+    }
+});
+*/
+
+
+
+
+
